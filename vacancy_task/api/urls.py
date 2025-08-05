21e549196f6635e8c912398471c6616ca1cb6515
Данил Тиводар <danilkativodar@gmail.com>
2025-08-05 16:11:23 +0300

    Add Vacancy Model

--- vacancy_task/api/urls.py
+++ vacancy_task/api/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 
 
-router.register()
+# router.register()
 
 urlpatterns = [
     path('', include(router.urls)),
--- vacancy_task/vacancies/models.py
+++ vacancy_task/vacancies/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+VACANCY_STATUS = '{vacancy} со статусом {status}'
+
+
+class Vacancy(models.Model):
+
+    STATUS_CHOICES = (
+        ('archive', 'ARCHIVE'),
+        ('active', 'ACTIVE'),
+    )
+
+    title = models.CharField('Заголовок', max_length=32)
+    status = models.CharField(
+        'Статус', choices=STATUS_CHOICES, default='active', max_length=16,
+    )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='vacancies',
+        verbose_name='Владелец',
+    )
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return VACANCY_STATUS.format(vacancy=self.title, status=self.status)
